fc6546a0a9cb59fc82038012704ba28309ba4fe0
Promise Obioma <proekene16@gmail.com>
2024-07-09 01:54:33 +0100

    update on ui

--- src/homePage/section2/actionButtons/CheckoutModal.jsx
+++ src/homePage/section2/actionButtons/CheckoutModal.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export default function CheckoutModal({ ispay, setIsPay }) {
   const continueShopping = () => {
     setIsPay(!ispay);
-  };                                  
+  };
   return (
     <div className="w-full  relative   h-auto flex justify-center items-center   ">
       {/* Overall card */}
@@ -15,12 +15,16 @@ export default function CheckoutModal({ ispay, setIsPay }) {
           {/* Order summary ------------------------------------------------------------------------- */}
           <div className="py-[48px] px-[24px] bg-[#FDFDFD] rounded-[16px] w-full ">
             <div className=" lg:w-[28vw] lg:h-[40vh]  flex flex-col px-[20px] lg:pt-[60px] items-center">
-              <h1 className="text-[2.5vw] font-extrabold text-2xl ">Order Summary</h1>
+              <h1 className="text-[2.5vw] font-extrabold text-2xl ">
+                Order Summary
+              </h1>
               <div className="flex">
                 <span>
                   <img src={"/"} alt="" />
                 </span>
-                <span className="text-[16px] font-semibold text-2xl">{"name of product"}</span>
+                <span className="text-[16px] font-semibold text-2xl">
+                  {"name of product"}
+                </span>
               </div>
               <p className="text-[2.5vw] text-3xl">
                 <span>Total:</span>
@@ -32,34 +36,37 @@ export default function CheckoutModal({ ispay, setIsPay }) {
           </div>
           {/* Payment Sections -------------------------------------------------------------------------------- */}
           <div className="bg-[#FDFDFD] rounded-[16px]  py-[48px] px-[24px] w-full ">
-            <div className=" lg:w-[28vw] w-full">
-              <h1>Make Payment</h1>
+            <div className=" lg:w-[28vw] w-full text-center">
+              <h1 className=" font-extrabold text-2xl">Make Payment</h1>
               {/* Form starts */}
               <div>
-                <form action="POST" className=" flex items-center   flex-col gap-3 ">
-                  
+                <form
+                  action="POST"
+                  className=" flex items-center   flex-col gap-3 "
+                >
+                  <input
+                    type="text"
+                    placeholder="FullName"
+                    className="input px-4 py-2 shadow-xl  border-0  outline-none bg-slate-100 rounded-lg w-full input-bordered "
+                  />
+
+                  <input
+                    type="text"
+                    placeholder="Card details"
+                    className="input px-4 py-2 shadow-xl border-0 outline-none bg-slate-100 w-full rounded-lg input-bordered  "
+                  />
+
+                  <div className="flex">
                     <input
                       type="text"
-                      placeholder="FullName"
-                      className="input px-4 py-2 shadow-xl  border-0  outline-none bg-slate-100 rounded-lg w-full input-bordered "
+                      placeholder="MM/YY"
+                      className="input input-bordered input-primary w-full max-w-xs"
                     />
-                  
-                  
-                  <input
+                    <input
                       type="text"
-                      placeholder="Card details"
-                      className="input px-4 py-2 shadow-xl border-0 outline-none bg-slate-100 w-full rounded-lg input-bordered  "
+                      placeholder="CVV"
+                      className="input py-2 px-2 input-bordered input-primary w-full max-w-xs"
                     />
-                 
-                  <div className="flex flex-col">
-                    <div>
-                      <label htmlFor="">MM/YY</label>
-                      <input className="border border-black" type="" />
-                    </div>
-                    <div>
-                      <label htmlFor="">CVV</label>
-                      <input className="border border-black" type="" />
-                    </div>
                   </div>
                   <div>
                     <button type="submit">Pay Now</button>
